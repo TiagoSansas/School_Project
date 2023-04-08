4bcfcaf9b2aea107c30678eb6f2edd3de109f1a7
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-04-08 09:31:06 -0300

    Create Service and Resource delete

--- backend/src/main/java/com/project/school/serivce/ClassService.java
+++ backend/src/main/java/com/project/school/serivce/ClassService.java
@@ -3,6 +3,7 @@ package com.project.school.serivce;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -51,9 +52,17 @@ public class ClassService {
 			return new ClassDTO(entity);
 			
 		}catch(EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Entity not Found" + id);
+			throw new ResourceNotFoundException("Id not found" + id);
 		}
 		
+	}
+	
+	public void delete (Long id) {
+		try {
+			repository.deleteById(id);;
+		}catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
 	}
 
 	private void copyDtoToEntity(ClassDTO dto, Class entity) {
