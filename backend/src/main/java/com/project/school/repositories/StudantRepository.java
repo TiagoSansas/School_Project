0a5c825aee3d6e4deec0052e685c34c9e5d3d649
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-04-08 19:04:28 -0300

    Refactor the in StudantRepository

--- backend/src/main/java/com/project/school/repositories/StudantRepository.java
+++ backend/src/main/java/com/project/school/repositories/StudantRepository.java
@@ -3,8 +3,10 @@ package com.project.school.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.project.school.entites.Studant;
+
 
 @Repository
-public interface StudantRepository extends JpaRepository<StudantRepository,Long> {}
+public interface StudantRepository extends JpaRepository<Studant,Long> {}
 
 
